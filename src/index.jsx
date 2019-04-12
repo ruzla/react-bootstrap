cf6332ab8bd319fb87f506ecdb2af31e676983fd
Russell <russelldoe@hotmail.co.uk>
2019-04-12 19:13:54 +0100

    location data complete

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,7 @@
 import 'raf/polyfill';
 import React from 'react';
 import { render } from 'react-dom';
+import App from '../src/components/app';
+import { location } from '../src/data/forcast.json';
 
-render(<h1>Hello World!</h1>, document.getElementById('root'));
+render(<App location={location} />, document.getElementById('root'));

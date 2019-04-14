b5022e50466a1e4895f6be1387794d456d1f06a7
Russell <russelldoe@hotmail.co.uk>
2019-04-14 20:49:50 +0100

    external data and lifecycle methods complete

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,18 +1,33 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import LocationDetails from './location-details';
 import ForecastSummaries from './forecast-summaries';
 import ForecastDetails from './forecast-details';
 import '../styles/app.scss';
 import '../styles/forecast-summaries.scss';
+import axios from 'axios';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      selectedDate: this.props.forecasts[0].date,
+      selectedDate: 0,
+      forecasts: [],
+      location: {
+        city: '',
+        country: '',
+      },
     };
     this.handleForecastSelect = this.handleForecastSelect.bind(this);
+  }
+
+  componentDidMount() {
+    axios.get('https://mcr-codes-weather.herokuapp.com/forecast')
+      .then(response => {
+        this.setState({
+          forecasts: response.data.forecasts,
+          location: response.data.location,
+        });
+      });
   }
 
   handleForecastSelect(date) {
@@ -22,29 +37,20 @@ class App extends React.Component {
   }
 
   render() {
-    const selectedForecast = this.props.forecasts.find(forecast => forecast.date === this.state.selectedDate);
+    const selectedForecast = this.state.forecasts.find(forecast => forecast.date === this.state.selectedDate);
     return (
       <div className="forecast">
         <LocationDetails
-          city={this.props.location.city}
-          country={this.props.location.country}
+          city={this.state.location.city}
+          country={this.state.location.country}
         />
-        <ForecastSummaries forecasts={this.props.forecasts} onForecastSelect={this.handleForecastSelect} />
-        <ForecastDetails forecast={selectedForecast} />
+        <ForecastSummaries forecasts={this.state.forecasts} onForecastSelect={this.handleForecastSelect} />
+        {
+            selectedForecast && <ForecastDetails forecast={selectedForecast} />
+        }
       </div>
     );
   }
 }
-
-
-App.propTypes = {
-  location: PropTypes.shape({
-    city: PropTypes.string,
-    country: PropTypes.string,
-  }).isRequired,
-  forecasts: PropTypes.array.isRequired,
-
-};
-
 
 export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,5 @@ import 'raf/polyfill';
 import React from 'react';
 import { render } from 'react-dom';
 import App from '../src/components/app';
-import { location, forecasts } from '../src/data/forecast.json';
 
-render(<App location={location} forecasts={forecasts} />, document.getElementById('root'));
+render(<App />, document.getElementById('root'));
